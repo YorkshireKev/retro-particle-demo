7475ce351b6b4656c987a6d85b12a771c8ac97d0
Kev Ellis <kev@ellis-online.co.uk>
2015-09-14 20:50:39 +0100

    Initial push

---
+++ js/particle.js
@@ -0,0 +1,136 @@
+/*jslint browser: true*/
+/*global THREE, Stats*/
+
+/*
+---------------------------------------------------------------------------
+
+PARTICLE DEMO
+=============
+
+Written By Kevin Ellis September 2015.
+
+An experiment in particles (aka sprites) inspired by the Atari ST Demo
+scene of the late 1980s.
+
+---------------------------------------------------------------------------
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Kevin Ellis
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---------------------------------------------------------------------------
+*/
+
+function virtualSlotMachine() {
+  'use strict';
+
+  function initStats() {
+    var stats = new Stats();
+    stats.setMode(0); // 0: fps, 1: ms
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+    document.body.appendChild(stats.domElement);
+    return stats;
+  }
+
+  var stats = initStats();
+
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor(new THREE.Color(0xB8EDFF, 1.0));
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  renderer.shadowMapEnabled = true;
+  // create the ground plane
+  var planeGeometry = new THREE.PlaneBufferGeometry(150, 150);
+  var planeMaterial = new THREE.MeshLambertMaterial({
+    color: 0xBBFF00
+  });
+
+  var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.receiveShadow = true;
+  // rotate and position the plane
+  plane.rotation.x = -0.5 * Math.PI;
+  plane.position.x = 0;
+  plane.position.y = -15;
+  plane.position.z = 0;
+  // add the plane to the scene
+  scene.add(plane);
+
+  // position and point the camera to the center of the scene
+  camera.position.x = 25;
+  camera.position.y = 10;
+  camera.position.z = 60;
+  camera.lookAt(scene.position);
+
+  //Add an ambient light
+  var ambientLight = new THREE.AmbientLight(0x333333);
+  scene.add(ambientLight);
+
+  // add spotlight for the shadows
+  var spotLight = new THREE.SpotLight(0xffffff);
+  spotLight.position.set(20, 50, 100);
+  spotLight.castShadow = true;
+  scene.add(spotLight);
+
+
+  //Camera controls
+  var orbitControls = new THREE.OrbitControls(camera);
+  orbitControls.rotateSpeed = 1.0;
+  orbitControls.zoomSpeed = 1.0;
+  orbitControls.panSpeed = 1.0;
+  orbitControls.noPan = true;
+  orbitControls.maxPolarAngle = Math.PI / 2;
+  orbitControls.minAzimuthAngle = -Math.PI / 2;
+  orbitControls.maxAzimuthAngle = Math.PI / 2;
+  orbitControls.minDistance = 25;
+  orbitControls.maxDistance = 100;
+
+
+  function onResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+  window.addEventListener('resize', onResize, false);
+
+  // add the output of the renderer to the html element
+  document.body.appendChild(renderer.domElement);
+
+
+  var clock = new THREE.Clock();
+
+  function renderScene() {
+    stats.update();
+    var delta = clock.getDelta();
+    orbitControls.update(delta);
+
+    window.requestAnimationFrame(renderScene);
+    renderer.render(scene, camera);
+  }
+
+  renderScene();
+} //end virtualSlotMachine
+
+window.onload = virtualSlotMachine;
